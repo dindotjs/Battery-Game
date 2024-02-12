f4db075a2e07720992414aca94225e644114a479
dindotjs <danielwragg07@gmail.com>
2024-02-12 19:29:50 +0000

    jumping

--- Battery-Game/Assets/Scripts/PlayerMovement.cs
+++ Battery-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float runSpeed = 2f;
-    float maxSpeed = 8f;
-    float friction = 2f;
+    float runSpeed = 4f;
+    float maxSpeed = 6f;
+    float turnAroundSpeed = 8f;
+    float acceleration;
+    float friction = 8f;
     float tolerence = 0.5f;
 
     bool grounded = true;
-    float jumpSpeed = 100f;
+    bool onGround;
+    float jumpSpeed = 400f;
+    float groundDistance = 0.5f;
+    int groundLayer;
+    float coyoteTime;
+    static float coyoteTimeConst = 0.1f;
+    float jumpBuffer;
+    static float jumpBufferConst = 0.5f;
+    bool spaceDown;
+    bool jumping = false;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        groundLayer = LayerMask.GetMask("Ground");
+        coyoteTime = coyoteTimeConst;
+        jumpBuffer = jumpBufferConst;
+        acceleration = runSpeed;
     }
 
     // Update is called once per frame
@@ -28,35 +43,97 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
-        if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) {
             return;
         }
         if (Input.GetKey(KeyCode.D) && rb.velocity.x < maxSpeed)
         {
-            rb.AddForce(Vector2.right * runSpeed);
+            if(rb.velocity.x < 0) { acceleration = turnAroundSpeed; } 
+            rb.AddForce(Vector2.right * acceleration);
             return;
         }
         if (Input.GetKey(KeyCode.A) && rb.velocity.x > -maxSpeed)
         {
-            rb.AddForce(Vector2.left * runSpeed);
+            if(rb.velocity.x > 0) { acceleration = turnAroundSpeed; }
+            rb.AddForce(Vector2.left * acceleration);
             return;
         }
-        if(rb.velocity.x > tolerence)
+
+        //friction
+        if(rb.velocity.x < tolerence && rb.velocity.x > -tolerence)
         {
-            rb.AddForce(Vector2.left * friction);
+            rb.velocity = new Vector2(0f, rb.velocity.y);
             return;
         }
-        if(rb.velocity.x < -tolerence)
+        else if(rb.velocity.x > friction * Time.deltaTime)
         {
-            rb.AddForce(Vector2.right * friction);
+            rb.velocity = new Vector2(rb.velocity.x - friction, rb.velocity.y);
             return;
         }
+        else if(rb.velocity.x < friction * Time.deltaTime)
+        {
+            rb.velocity = new Vector2(rb.velocity.x + friction, rb.velocity.y);
+            return;
+        }
+        acceleration = runSpeed;
     }
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, groundLayer);
+        float distance = Mathf.Abs(hit.point.y - transform.position.y);
+        Debug.Log(distance);
+
+        if(distance < groundDistance) { onGround = true; }
+        else { onGround = false; }
+
+        if (onGround)
         {
+            grounded = true;
+            coyoteTime = coyoteTimeConst;
+        }
+
+        else if (!onGround)
+        {
+            coyoteTime -= Time.deltaTime;
+            if (coyoteTime <= 0f)
+            {
+                grounded = false;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceDown = true;
+            jumpBuffer = jumpBufferConst;
+        }
+
+        if (spaceDown)
+        {
+            jumpBuffer -= Time.deltaTime;
+            if (jumpBuffer <= 0f)
+            {
+                spaceDown = false;
+            }
+        }
+
+        if (spaceDown && grounded && !jumping)
+        {
+            coyoteTime = 0f;
+            jumpBuffer = 0f;
+            jumping = true;
             rb.AddForce(Vector2.up * jumpSpeed);
+            rb.gravityScale = 1.5f;
+        }
+
+        if (jumping && rb.velocity.y <= 0f || !Input.GetKey(KeyCode.Space))
+        {
+            jumping = false;
+            rb.gravityScale = 2.5f;
+        }
+
+        if(!jumping && grounded)
+        {
+            rb.gravityScale = 2f;
         }
     }
 }
